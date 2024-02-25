2b6dc33f4460643604709465630554866f028468
Mahmoud Reda <115508120+MahmoudReda11@users.noreply.github.com>
2024-02-25 21:49:18 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,65 @@
+let patharr = window.location.pathname.split("/");
+patharr.splice(-1);
+let path = patharr.join("/");
+let host = window.location.protocol + "//" + window.location.host + path;
+let redirectValue = document.getElementById("redDir").value;
+document.getElementById("redDir").value = `${host}/thanks.html`;
+
+
+
+
+
+
+const name = document.getElementById('text')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+
+  if (password.value.length < 11) {
+    messages.push('رقم الهاتف غير صحيح !')
+  }
+
+  if (password.value.length > 11) {
+    messages.push('رقم الهاتف غير صحيح !')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
+
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2000, // وقت التأخير بين الشرائح بالمللي ثانية
+        disableOnInteraction: false, // تعطيل التشغيل التلقائي عند التفاعل مع المستخدم (باستخدام الماوس)
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints:{
+    1200:{
+      slidesPerView : 4,
+    },
+    800:{
+      slidesPerView : 3,
+      spaceBetween: 30,
+    },
+    500 :{
+      slidesPerView : 1,
+    },
+    0 :{
+      slidesPerView : 1,
+      spaceBetween: 30,
+    }
+  }
+});
